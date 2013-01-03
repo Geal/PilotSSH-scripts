e0403f2e5aa33853e31e34cacbc7d32453423c3f
Geoffroy Couprie <geo.couprie@gmail.com>
2013-01-03 17:42:38 +0100

    simplify the bash strings

--- power/power_halt.sh
+++ power/power_halt.sh
@@ -2,7 +2,7 @@
 halt > /dev/null 2>&1
 OUT=$?
 if [ $OUT -eq 0 ];then
-  echo "{ \"version\": 1, \"title\": \"Halt\", \"type\":\"status\", \"status\":\"ok\", \"message\":\"The server will halt\"}"
+  echo '{ "version": 1, "title": "Halt", "type":"status", "status":"ok", "message":"The server will halt"}'
 else
-  echo "{ \"version\": 1, \"title\": \"Halt\", \"type\":\"status\", \"status\":\"failed\", \"message\":\"could not halt the server\"}"
+  echo '{ "version": 1, "title": "Halt", "type":"status", "status":"failed", "message":"could not halt the server"}'
 fi
--- power/power_reboot.sh
+++ power/power_reboot.sh
@@ -2,7 +2,7 @@
 reboot > /dev/null 2>&1
 OUT=$?
 if [ $OUT -eq 0 ];then
-  echo "{ \"version\": 1, \"title\": \"Reboot\", \"type\":\"status\", \"status\":\"ok\", \"message\":\"rebooting the server\"}"
+  echo '{ "version": 1, "title": "Reboot", "type":"status", "status":"ok", "message":"rebooting the server"}'
 else
-  echo "{ \"version\": 1, \"title\": \"Reboot\", \"type\":\"status\", \"status\":\"failed\", \"message\":\"could not reboot the server\"}"
+  echo '{ "version": 1, "title": "Reboot", "type":"status", "status":"failed", "message":"could not reboot the server"}'
 fi
